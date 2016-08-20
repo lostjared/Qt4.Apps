3736e054f454a91f2734998942b126bc276b3561
lostjared <jaredbruni@gmail.com>
2016-08-20 14:23:50 -0700

    updated

--- Hello_World/test.cpp
+++ Hello_World/test.cpp
@@ -2,14 +2,11 @@
 #include<iostream>
 #include"test_window.h"
 
-
-
 int main(int argc, char **argv) {
-    
     QApplication app(argc, argv);
     Window1 win1;
     win1.show();
     app.exec();
-    
     return 0;
 }
+
